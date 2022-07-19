04779e5cf0eb2e8b6a1e9a7c78d75adcdad10b65
eldimious <botsaris.d@gmail.com>
2022-07-19 11:35:33 +0300

    ECOM-0: remove panache because of https://github.com/quarkusio/quarkus/issues/23804\#issuecomment-1125529334

--- backend/services/OrderService/src/main/kotlin/com/arconsis/data/orders/OrdersDataStore.kt
+++ backend/services/OrderService/src/main/kotlin/com/arconsis/data/orders/OrdersDataStore.kt
@@ -4,16 +4,12 @@ import com.arconsis.domain.orders.CreateOrder
 import com.arconsis.domain.orders.Order
 import com.arconsis.domain.orders.OrderStatus
 import io.smallrye.mutiny.Uni
-import io.smallrye.mutiny.coroutines.asUni
-import io.smallrye.mutiny.coroutines.awaitSuspending
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
 import org.hibernate.reactive.mutiny.Mutiny
 import java.util.*
 import javax.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
-class OrdersDataStore(private val sessionFactory: Mutiny.SessionFactory,) {
+class OrdersDataStore {
 
 	fun updateOrderStatus(orderId: UUID, status: OrderStatus, session: Mutiny.Session): Uni<Order> {
 		return session.find(OrderEntity::class.java, orderId)
@@ -37,28 +33,4 @@ class OrdersDataStore(private val sessionFactory: Mutiny.SessionFactory,) {
 		return session.find(OrderEntity::class.java, orderId)
 			.map { orderEntity -> orderEntity.toOrder() }
 	}
-
-
-	suspend fun <T> withTransaction(block: Mutiny.Session.(Mutiny.Transaction) -> Uni<T>): T = withRetryingTransaction { transaction ->
-		block(transaction)
-	}
-
-	suspend fun <T> coWithTransaction(block: suspend Mutiny.Session.(Mutiny.Transaction) -> T): T = coroutineScope {
-		withRetryingTransaction { transaction ->
-			async {
-				block(transaction)
-			}.asUni()
-		}
-	}
-
-	private suspend fun <T> withRetryingTransaction(block: Mutiny.Session.(Mutiny.Transaction) -> Uni<T>): T = withRetry {
-		withTransaction { session, transaction -> session.block(transaction) }
-	}
-
-	private suspend fun <T> withRetry(block: suspend Mutiny.SessionFactory.() -> Uni<T>): T =
-		sessionFactory.block()
-			.onFailure()
-			.retry()
-			.atMost(0)
-			.awaitSuspending()
 }
